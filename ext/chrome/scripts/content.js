096e8b206cfcd252466be1a4c505afb5fab895da
Brad Kimmel <intrepidca@gmail.com>
2025-03-06 22:53:41 -0500

    Add lookup panel to Chrome extension.

--- ext/chrome/scripts/content.js
+++ ext/chrome/scripts/content.js
@@ -2,6 +2,7 @@ var bookId = null;
 var video = null;
 var cues = null;
 var updateId = null;
+var selectedWord = null;
 
 function removeCueContainer() {
   var ctr = document.getElementById('lit-cue-container');
@@ -38,9 +39,44 @@ async function onPlay() {
   if (updateId == null) {
     updateId = setInterval(update, 50);
   }
+  const ctr = document.getElementById('lit-panel-container');
+  if (ctr != null) {
+    ctr.classList.remove('lit-show');
+  }
 }
 
-var wasPausedBeforeCueHover = false;
+async function onWordClick(e) {
+  pauseAfterCueHovering = true;
+  if (selectedWord != null) {
+    selectedWord.classList.remove('lit-selected');
+  }
+  selectedWord = e.target;
+  selectedWord.classList.add('lit-selected');
+  const offset = Number(e.target.getAttribute('data-lit-word-offset'));
+  const url = `http://localhost:5080/api/books/${bookId}/words/${offset}`;
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    console.warn(`Could not fetch word info at offset ${offset}: ${await resp.body()}`);
+    return;
+  }
+  const json = await resp.json();
+  var words = Array();
+  for (const dep of json.deps) {
+    if (words.includes(dep.text)) {
+      continue;
+    }
+    words.push(dep.text);
+  }
+  if (!words.includes(json.text)) {
+    words.push(json.text);
+  }
+  const list = words.join(',');
+  const defineURL = `http://localhost:5080/define/${list}/edit`;
+  window.open(defineURL, 'lit-define');
+  document.getElementById('lit-panel-container').classList.add('lit-show');
+}
+
+var pauseAfterCueHovering = false;
 var cueHovering = false;
 var cueHoveringTimeout = null;
 
@@ -53,7 +89,7 @@ async function onCueMouseOver(e) {
     return;
   }
   cueHovering = true;
-  wasPausedBeforeCueHover = video.paused;
+  pauseAfterCueHovering = video.paused;
   video.pause();
 }
 
@@ -63,7 +99,7 @@ async function onCueMouseOut(e) {
   }
   cueHoveringTimeout = setTimeout(function() {
     cueHovering = false;
-    if (!wasPausedBeforeCueHover) {
+    if (!pauseAfterCueHovering) {
       video.play();
     }
   }, 250);
@@ -139,6 +175,7 @@ async function update() {
           }
           span.classList.add(`lit-max-status-${token.word.max_status}`);
           span.setAttribute('data-lit-word-offset', `${token.word.offset}`);
+          span.addEventListener('click', onWordClick);
           node = span;
         }
         div.appendChild(node);
@@ -165,9 +202,7 @@ async function update() {
       fetch(`http://localhost:5080/api/books/${bookId}/words/${offset}`)
         .then((resp) => resp.json())
         .then((word) => {
-          const content = generateTooltipContent(word);
-          console.info(content);
-          instance.setContent(content);
+          instance.setContent(generateTooltipContent(word));
         })
         .catch((error) => {
           console.warn("Failed to load tooltip");
@@ -325,7 +360,7 @@ async function initVideo() {
 }
 
 async function init(url) {
-  var e = document.getElementById('lit-term-panel');
+  var e = document.getElementById('lit-panel-container');
   if (!await setupVideoPage(url)) {
     if (e) {
       e.remove();
@@ -337,9 +372,45 @@ async function init(url) {
     return;
   }
   e = document.createElement('div');
-  e.setAttribute("id", "lit-term-panel");
-  e.innerHTML = "Test content";
+  e.setAttribute("id", "lit-panel-container");
+  e.innerHTML = `
+    <div class="lit-grid-container" style="grid-template-columns: 2fr 5px 1fr;">
+      <div id="lit-panel-background"></div>
+      <div id="lit-vertical-gutter" class="lit-gutter">
+        <div id="lit-toggle-fullscreen" class="lit-gutter-button" onmousedown="event.stopPropagation(); event.preventDefault();">&#x25C0;</div>
+        <div id="lit-close" class="lit-gutter-button" onmousedown="event.stopPropagation(); event.preventDefault();">&#x1F5D9;</div>
+      </div>
+      <div id="lit-panel">
+        <iframe name="lit-define"></iframe>
+      </div>
+    </div>
+  `;
   document.body.appendChild(e);
+  const grid = e.querySelector('.lit-grid-container');
+  const btn = e.querySelector('#lit-toggle-fullscreen');
+  btn.addEventListener('click', function() {
+    if (e.classList.contains('lit-fullscreen')) {
+      grid.setAttribute('style', grid.getAttribute('data-lit-style'));
+      grid.removeAttribute('data-lit-style');
+      e.classList.remove('lit-fullscreen');
+      btn.innerHTML = '&#x25C0;';
+    } else {
+      e.classList.add('lit-fullscreen');
+      btn.innerHTML = '&#x25B6;';
+      grid.setAttribute('data-lit-style', grid.getAttribute('style'));
+      grid.removeAttribute('style');
+    }
+  });
+  e.querySelector('#lit-close').addEventListener('click', function() {
+    document.getElementById('lit-panel-container').classList.remove('lit-show');
+  });
+
+  Split({
+    columnGutters: [{
+      track: 1,
+      element: document.getElementById('lit-vertical-gutter')
+    }]
+  });
 }
 
 init(document.URL);
